3d0d2664261b1eebb16de49813913b04ac693ed3
Jordibz23 <0333191025@unjfsc.edu.pe>
2023-05-19 11:29:08 -0500

    funciona con spring security

--- src/main/java/escalafon/com/prueba/security/SecurityConfig.java
+++ src/main/java/escalafon/com/prueba/security/SecurityConfig.java
@@ -15,8 +15,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration //le indica al contenedor de spring que esta es una clase de seguridad al momento de arrancar la aplicación
 @EnableWebSecurity //se activa la seguridad web en nuestra aplicación
